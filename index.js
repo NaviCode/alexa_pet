d3df016cea8390507dfa65a361fa70fd4e2398f2
Fernando Sanchez <sanchez5047@gmail.com>
2017-08-01 00:05:22 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,36 @@
+/*jshint esversion: 6 */
+const Alexa     = require('alexa-sdk');
+
+exports.handler = function(event, context, callback){
+    const alexa = Alexa.handler(event,context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function() {
+        this.emit(':ask', 'Welcome to Alexa Day care!');
+    },
+    'Test':function(){
+        this.emit(':tell','Test complete');
+    },
+    'AMAZON.StopIntent': function() {
+        // State Automatically Saved with :tell
+        this.emit(':tell', `Goodbye.`);
+    },
+    'AMAZON.CancelIntent': function() {
+        // State Automatically Saved with :tell
+        this.emit(':tell', `Goodbye.`);
+    },
+    'SessionEndedRequest': function() {
+        // Force State Save When User Times Out
+        this.emit(':saveState', true);
+    },
+    'AMAZON.HelpIntent': function() {
+        this.emit(':ask', `You can tell me the name of a musical artist and I will say it back to you.  Who would you like me to find?`, `Who would you like me to find?`);
+    },
+    'Unhandled': function() {
+        this.emit(':ask', `You can tell me the name of a musical artist and I will say it back to you.  Who would you like me to find?`, `Who would you like me to find?`);
+    }
+
+};
