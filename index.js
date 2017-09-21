1c57a26b11916300e2529322c1e701d300450093
Fernando Sanchez <sanchez5047@gmail.com>
2017-09-20 22:21:21 -0400

    can create pets

--- index.js
+++ index.js
@@ -1,64 +1,94 @@
 /*jshint esversion: 6 */
-const Alexa     = require('alexa-sdk');
-const Pet       = require('./modals/pet');
-const Request   = require('request');
+const Alexa = require('alexa-sdk');
+const Pet = require('./modals/pet');
+const request = require('request');
 
-exports.handler = function(event, context, callback){
-    const alexa = Alexa.handler(event,context);
-    if(event.session.user.accessToken == undefined){
-        alexa.emit(':tellWithLinkAccountCard','to start using this skill, please use the companion app to authenticate on Amazon.');
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context);
+    if (event.session.user.accessToken == undefined) {
+        alexa.emit(':tellWithLinkAccountCard', 'to start using this skill, please use the companion app to authenticate on Amazon.');
     }
-    profileUrl = 'https://api.amazon.com/user/profile?access_token=';
-    profileUrl += this.event.session.user.accessToken;
-    name = '';
-    request.get(profileUrl,function(err,res,body){
-        if(res.statusCode == 200){
-            let profile = JSON.parse(body);
-            console.log(profile);
-            name  = profile['name'];
-        }
-    });
-    console.log(name);
+    // console.log(name);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 const handlers = {
-    'LaunchRequest': function() {
-        this.emit(':ask', 'Welcome to Alexa Day care!');
+    'LaunchRequest': function () {
+        profileUrl = 'https://api.amazon.com/user/profile?access_token=';
+        profileUrl += this.event.session.user.accessToken;
+        petUrl = "https://sleepy-beach-44231.herokuapp.com/api/v1/pets";
+        name = '';
+        /* profile object ={user_id, name, email} */
+        request.get(profileUrl, (err, res, body) => {
+            if (res.statusCode === 200) {
+                let profile = JSON.parse(body);
+                name = profile['name'];
+                name = name.split(' ');
+                request.get(petUrl, (err, res, body) => {
+                    if (res.statusCode === 200) {
+                        if (JSON.parse(body).length == 0) {
+                            this.emit(':ask', `Welcome ${name[0]}, to our doggie daycare! Looks like you don't have any pets. What would you like to name your new puppy?`);
+                        }
+                        else {
+                            this.emit(':ask', `Welcome back ${name[0]}, to our doggie daycare!`);
+                        }
+                    }
+                });
+            }
+        });
     },
     /* My Intents */
 
-    'GetHealthIntent':function(){
+    'GetHealthIntent': function () {
         Pet.get_health(this);
     },
-    'GetHungerIntent': function(){
+    'GetHungerIntent': function () {
         Pet.get_hunger(this);
     },
-    "FeedIntent": function(){
+    "FeedIntent": function () {
         Pet.feed(this);
     },
-    "ExerciseIntent": function(){
+    "ExerciseIntent": function () {
         Pet.exercise(this);
     },
-    
+    "NamePetIntent": function () {
+        this.attributes['myPetName'] = this.event.request.intent.slots.petName.value;
+        let body = {
+            "pet": {
+                "name": this.attributes['myPetName'],
+                "user_id": 1
+            }
+        };
+        name = this.attributes['myPetName'];      
+        var options = { method: 'POST',
+        url: 'https://sleepy-beach-44231.herokuapp.com/api/v1/pets',
+        headers: 
+         { 'postman-token': 'eabce58a-68c1-c3d8-4aa2-f958769a49fe',
+           'cache-control': 'no-cache',
+           'content-type': 'application/json' },
+        body: { pet: { name: name, user_id: 1 } },
+        json: true };
+
+        Pet.create(this,body,name,options);
+    },
     /* My Intents */
-    'AMAZON.StopIntent': function() {
+    'AMAZON.StopIntent': function () {
         // State Automatically Saved with :tell
         this.emit(':tell', `Goodbye.`);
     },
-    'AMAZON.CancelIntent': function() {
+    'AMAZON.CancelIntent': function () {
         // State Automatically Saved with :tell
         this.emit(':tell', `Goodbye.`);
     },
-    'SessionEndedRequest': function() {
+    'SessionEndedRequest': function () {
         // Force State Save When User Times Out
         this.emit(':saveState', true);
     },
-    'AMAZON.HelpIntent': function() {
-        this.emit(':ask', `You can tell me the name of a musical artist and I will say it back to you.  Who would you like me to find?`, `Who would you like me to find?`);
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', `In this doggie daycare you can say feed, walk, or play with your pet!`);
     },
-    'Unhandled': function() {
+    'Unhandled': function () {
         this.emit(':ask', `Bark? Bark? I do not know those human words. I mean, bark bark.`);
     }
 };
--- modals/pet.js
+++ modals/pet.js
@@ -1,56 +1,66 @@
 /*jshint esversion: 6 */
-exports.get_health = function(alexa){
+exports.get_health = function (alexa) {
     let hp = Math.floor(100 * Math.random());
-    console.log("HP FOR INSTANCE: ",hp);
+    console.log("HP FOR INSTANCE: ", hp);
     alexa.emit(":tell", healthGate(hp));
 };
-exports.get_hunger = function(alexa){
+exports.get_hunger = function (alexa) {
     let hunger = Math.floor(101 * Math.random());
-    alexa.emit(":tell",hungerGate(hunger));
+    alexa.emit(":tell", hungerGate(hunger));
 };
-exports.feed = function(alexa){
+exports.feed = function (alexa) {
 
 };
-exports.exercise = function(alexa){
+exports.exercise = function (alexa) {
 
 };
-const exercise = function(){
-    return `<emphasis level="strong">Finally.<break time='.5s'/></emphasis> We're finally going for a walk.`
+exports.create = function (alexa, data, name, options) {
+    var request = require("request");
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log("THE BODY IS HERE", body);
+        alexa.emit(`:ask`, `Meet your new puppy, ${name}! Say hello!`);
+    });
+
+};
+const exercise = function () {
+    return `<emphasis level="strong">Finally.<break time='.5s'/></emphasis> We're finally going for a walk.`;
 };
 
-const hungerGate = function(hunger){
+const hungerGate = function (hunger) {
     switch (true) {
         case (hunger <= 10):
             return `Oh god. I'm about to die, please feed me!`
-        case (hunger <=50):
+        case (hunger <= 50):
             return `I'm feeling pretty weak, can you please feed me?`;
-        case (hunger <=80):
+        case (hunger <= 80):
             return `I'm content! Thank you for keeping me well fed!`
-        case (hunger <=100):
+        case (hunger <= 100):
             return `I. Am. So. Full. I'm so happy, thank you!`
         case (hunger > 101):
             return `Please, no more! I've had enough! No more food!`
     }
 };
 
-const exerciseGate = function(fitness){
-    switch(true){
-        
+const exerciseGate = function (fitness) {
+    switch (true) {
+
     }
 };
 
-const healthGate = function(hp){
-    switch(true){
-        case (hp <= 10): 
+const healthGate = function (hp) {
+    switch (true) {
+        case (hp <= 10):
             return `HP levels are critical. I'm about to die. <emphasis level="strong"> Please </emphasis> Do something about it!`;
         case (hp <= 50):
-            return `My health is at ${hp}, please help me. Feed, or play with me!`
+            return `My health is at ${hp}, please help me. Feed, or play with me!`;
         case (hp <= 89):
-            return `I'm okay, but I could always eat or play! My hp is at ${hp}`
-        case(hp >= 90):
-            return `Wow! I'm doing fantastic. You're such a good care-taker! My hp is at ${hp}`
+            return `I'm okay, but I could always eat or play! My hp is at ${hp}`;
+        case (hp >= 90):
+            return `Wow! I'm doing fantastic. You're such a good care-taker! My hp is at ${hp}`;
     }
-}
+};
 
 /* IDEA */
 /* Make it relevent to parents. Feed and exercise with the pet only when they complete chores. Make a parent portal */
